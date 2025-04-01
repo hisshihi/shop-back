b3f0e61a61f3c418ba8ae95f5cba83a2dbb5c9bf
hisshihi <hissoffc@gmail.com>
2025-04-01 22:26:59 +0500

    Обновить тест

--- db/sqlc/order_test.go
+++ db/sqlc/order_test.go
@@ -12,107 +12,108 @@ import (
 )
 
 func createRandomOrder(t *testing.T) Order {
-    user := createRandomUser(t)
-    arg := CreateOrderParams{
-        UserID:         user.ID,
-        TotalAmount:    fmt.Sprintf("%.2f", gofakeit.Price(50, 500)),
-        Status:         "pending",
-        PaymentMethod:  "card",
-        DeliveryStatus: NullOrderStatus{OrderStatus: OrderStatusProcessed, Valid: true},
-    }
+	user := createRandomUser(t)
+	arg := CreateOrderParams{
+		UserID:         user.ID,
+		TotalAmount:    fmt.Sprintf("%.2f", gofakeit.Price(50, 500)),
+		Status:         "pending",
+		PaymentMethod:  "card",
+		DeliveryStatus: NullOrderStatus{OrderStatus: OrderStatusProcessed, Valid: true},
+	}
 
-    order, err := testQueries.CreateOrder(context.Background(), arg)
-    require.NoError(t, err)
-    require.NotEmpty(t, order)
+	order, err := testQueries.CreateOrder(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, order)
 
-    require.Equal(t, arg.UserID, order.UserID)
-    require.Equal(t, arg.TotalAmount, order.TotalAmount)
-    require.Equal(t, arg.Status, order.Status)
-    require.Equal(t, arg.PaymentMethod, order.PaymentMethod)
-    require.Equal(t, arg.DeliveryStatus, order.DeliveryStatus)
-    require.NotZero(t, order.ID)
-    require.NotZero(t, order.CreatedAt)
+	require.Equal(t, arg.UserID, order.UserID)
+	require.Equal(t, arg.TotalAmount, order.TotalAmount)
+	require.Equal(t, arg.Status, order.Status)
+	require.Equal(t, arg.PaymentMethod, order.PaymentMethod)
+	require.Equal(t, arg.DeliveryStatus, order.DeliveryStatus)
+	require.NotZero(t, order.ID)
+	require.NotZero(t, order.CreatedAt)
 
-    return order
+	return order
 }
 
 func TestCreateOrder(t *testing.T) {
-    createRandomOrder(t)
+	createRandomOrder(t)
 }
 
 func TestGetOrderByID(t *testing.T) {
-    order1 := createRandomOrder(t)
-    order2, err := testQueries.GetOrderByID(context.Background(), order1.ID)
-    require.NoError(t, err)
-    require.NotEmpty(t, order2)
+	order1 := createRandomOrder(t)
+	order2, err := testQueries.GetOrderByID(context.Background(), order1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, order2)
 
-    require.Equal(t, order1.ID, order2.ID)
-    require.Equal(t, order1.UserID, order2.UserID)
-    require.Equal(t, order1.TotalAmount, order2.TotalAmount)
-    require.Equal(t, order1.Status, order2.Status)
-    require.Equal(t, order1.PaymentMethod, order2.PaymentMethod)
-    require.Equal(t, order1.DeliveryStatus, order2.DeliveryStatus)
-    require.WithinDuration(t, order1.CreatedAt, order2.CreatedAt, time.Second)
+	require.Equal(t, order1.ID, order2.ID)
+	require.Equal(t, order1.UserID, order2.UserID)
+	require.Equal(t, order1.TotalAmount, order2.TotalAmount)
+	require.Equal(t, order1.Status, order2.Status)
+	require.Equal(t, order1.PaymentMethod, order2.PaymentMethod)
+	require.Equal(t, order1.DeliveryStatus, order2.DeliveryStatus)
+	require.WithinDuration(t, order1.CreatedAt, order2.CreatedAt, time.Second)
 }
 
 func TestListOrders(t *testing.T) {
-    for range 10 {
-        createRandomOrder(t)
-    }
+	for range 10 {
+		createRandomOrder(t)
+	}
 
-    arg := ListOrdersParams{
-        Limit:  5,
-        Offset: 5,
-    }
+	arg := ListOrdersParams{
+		Limit:  5,
+		Offset: 5,
+	}
 
-    orders, err := testQueries.ListOrders(context.Background(), arg)
-    require.NoError(t, err)
-    require.Len(t, orders, 5)
+	orders, err := testQueries.ListOrders(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, orders, 5)
 
-    for _, order := range orders {
-        require.NotEmpty(t, order)
-    }
+	for _, order := range orders {
+		require.NotEmpty(t, order)
+	}
 }
 
 func TestCountOrders(t *testing.T) {
-    initialCount, err := testQueries.CountOrders(context.Background())
-    require.NoError(t, err)
+	initialCount, err := testQueries.CountOrders(context.Background())
+	require.NoError(t, err)
 
-    for range 10 {
-        createRandomOrder(t)
-    }
+	for range 10 {
+		createRandomOrder(t)
+	}
 
-    finalCount, err := testQueries.CountOrders(context.Background())
-    require.NoError(t, err)
-    require.Equal(t, initialCount+10, finalCount)
+	finalCount, err := testQueries.CountOrders(context.Background())
+	require.NoError(t, err)
+	require.Equal(t, initialCount+10, finalCount)
 }
 
 func TestUpdateOrderStatus(t *testing.T) {
-    order1 := createRandomOrder(t)
-    arg := UpdateOrderStatusParams{
-        ID:            order1.ID,
-        Status:        "completed",
-        DeliveryStatus: NullOrderStatus{OrderStatus: OrderStatusDelivered, Valid: true},
-    }
+	order1 := createRandomOrder(t)
+	arg := UpdateOrderStatusParams{
+		ID:             order1.ID,
+		Status:         string(OrderStatusCanceled),
+		DeliveryStatus: NullOrderStatus{OrderStatus: OrderStatusDelivered, Valid: true},
+	}
 
-    order2, err := testQueries.UpdateOrderStatus(context.Background(), arg)
-    require.NoError(t, err)
-    require.NotEmpty(t, order2)
+	order2, err := testQueries.UpdateOrderStatus(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, order2)
 
-    require.Equal(t, order1.ID, order2.ID)
-    require.Equal(t, arg.Status, order2.Status)
-    require.Equal(t, arg.DeliveryStatus, order2.DeliveryStatus)
-    require.WithinDuration(t, order1.CreatedAt, order2.CreatedAt, time.Second)
-    require.NotZero(t, order2.UpdatedAt)
+	require.Equal(t, order1.ID, order2.ID)
+	require.Equal(t, arg.Status, order2.Status)
+	require.Equal(t, arg.DeliveryStatus, order2.DeliveryStatus)
+	require.WithinDuration(t, order1.CreatedAt, order2.CreatedAt, time.Second)
+	require.NotZero(t, order2.UpdatedAt)
 }
 
 func TestDeleteOrder(t *testing.T) {
-    order1 := createRandomOrder(t)
-    err := testQueries.DeleteOrder(context.Background(), order1.ID)
-    require.NoError(t, err)
+	order1 := createRandomOrder(t)
+	err := testQueries.DeleteOrder(context.Background(), order1.ID)
+	require.NoError(t, err)
 
-    order2, err := testQueries.GetOrderByID(context.Background(), order1.ID)
-    require.Error(t, err)
-    require.EqualError(t, err, sql.ErrNoRows.Error())
-    require.Empty(t, order2)
+	order2, err := testQueries.GetOrderByID(context.Background(), order1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, order2)
 }
+
